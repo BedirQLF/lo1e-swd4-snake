5bc8632fcedd2085ad8dbe5b57b51df4289cdb83
BedirQLF <94383556+BedirQLF@users.noreply.github.com>
2022-10-04 13:16:12 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,1 +1,97 @@
-document.getElementById('snake');
+const canvas = document.getElementById("snake");
+
+const ctx = canvas.getContext('2d');
+
+let snakeX = 200;
+
+let snakeY = 200;
+
+let direction = null;
+
+
+
+function drawBackground() {
+
+ctx.fillStyle = 'black';
+
+ctx.fillRect(0, 0, 400, 400);
+
+}
+
+function drawSnake() {
+
+ctx.fillStyle = 'white';
+
+ctx.fillRect(snakeX, snakeY, 20, 20);
+
+}
+
+
+
+function update() {
+ 
+    if(direction == 'right') {
+
+    snakeX += 20;
+
+    } else if (direction == 'left') {
+
+    if (snakeX > 0) {
+
+        snakeX -= 20;
+
+    } else {
+
+        alert("Game Over");
+
+    }
+
+    } else if (direction == "up"){
+
+    snakeY -=20;
+
+    } else if(direction == "down") {
+
+    snakeY += 20;
+
+    }
+
+    drawBackground();
+
+    drawSnake();
+
+    }
+
+function ChangeDirection(event){
+
+    if(event.code == "ArrowUp") {
+
+        direction = "up";
+
+    } else if (event.code == "ArrowRight"){
+
+     direction = "right";
+
+    } else if (event.code == "ArrowLeft"){
+
+        direction = "left";
+
+
+
+    } else if (event.code == "ArrowDown"){
+
+        direction = "down";
+
+
+
+    }
+
+}
+
+drawBackground();
+
+drawSnake();
+
+setInterval(update, 500);
+
+addEventListener('keydown', ChangeDirection);
